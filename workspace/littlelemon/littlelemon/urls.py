4119b909a45327382a949544d40c1fcaf2ab0d27
meraj323 <mumtaz.meraj@gmail.com>
2025-11-08 02:40:02 -0400

    Added changes

--- workspace/littlelemon/littlelemon/urls.py
+++ workspace/littlelemon/littlelemon/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from myapp.views import MyViewSet, UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('restaurant/menu/',include(restaurant.urls')),
 ]
+
+
+
+
+
--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from .models import MenuItem
+from .serializers import MenuItemSerializer
+
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView): 
+queryset = MenuItem.objects.all() 
+serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):     
+queryset = MenuItem.objects.all() 
+serializer_class = MenuItemSerializer
+
 # Create your views here.
